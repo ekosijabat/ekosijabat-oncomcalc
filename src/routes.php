0523d7fabde523ef628ee106c26e07c07aed0784
ekosijabat <ekolesmanasijabat@gmail.com>
2017-02-23 16:39:18 +0700

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -4,5 +4,5 @@ Route::get('calculator', function() {
     echo 'Hello from the calculator package!';
 });
 
-Route::get('add/{a}/{b}', 'Devdojo\Calculator\CalculatorController@add');
-Route::get('subtract/{a}/{b}', 'Devdojo\Calculator\CalculatorController@subtract');
+Route::get('add/{a}/{b}', 'Ekosijabat\Oncomcalc\CalculatorController@add');
+Route::get('subtract/{a}/{b}', 'Ekosijabat\Oncomcalc\CalculatorController@subtract');
